4371e1d121066d65db2fd53ff3905f503f846c19
Yuhara-Tee <thanaponsaema@gmail.com>
2025-01-03 23:09:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var w = c.width = window.innerWidth,
     hh = h / 2,
     
     opts = {
-        strings: ['Orn', 'is!', 'Buffalo!!'],
+        strings: ['HAPPY', 'BIRTHDAY!', 'TO YOU!!'],
         charSize: 30,
         charSpacing: 35,
         lineHeight: 40,
